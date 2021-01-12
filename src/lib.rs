f189118bbcd4e80792e906071b8e2510ec73db5f
CyberWolf37 <borel.loic.37@gmail.com>
2021-01-12 20:33:07 +0100

    Add upgrade

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
+mod model;
+
 use std::sync::Arc;
 
 
+#[derive(Clone)]
 enum PipeState {
     NextState,
     RestartState,
@@ -14,61 +17,33 @@ trait PipeU: Eq {
     fn get_item(&self) -> &Self::Item;
 }
 
-struct PipeBox<T: PipeU> {
-    function_control: Option<Arc<dyn Fn(&T) -> PipeState + Send + Sync>>,
-    function_core: Option<Arc<dyn Fn(&T) -> PipeState + Send + Sync>>,
-    internal_state: PipeState,
-}
+trait PipeB<T: PipeU> {
+    fn function_control(&self) -> Option<Arc<dyn Fn(&T) -> PipeState + Send + Sync>>;
+    fn function_core(&self) -> Option<Arc<dyn Fn(&T) -> PipeState + Send + Sync>>;
+    fn internal_state(&self) -> PipeState;
 
-impl<T: PipeU> PipeBox<T> {
-    fn new() -> Self {
-        PipeBox {
-            function_control: None,
-            function_core: None,
-            internal_state: PipeState::ConsumeState,
-        }
-    }
-
-    fn internal_state(&self) -> &PipeState {
-        &self.internal_state
-    }
-
-    fn consume(&self,pipe_u: &T) -> PipeState {
-
-        match self.function_core.clone() {
-            Some(e) => (e.clone())(pipe_u),
-            None => panic!("Don't have function to consume"),
-        }
-    }
-
-    fn control(&self, pipe_u: &T) -> PipeState {
-        match self.function_control.clone() {
+    fn control(&self,pipe_u: &T) -> PipeState {
+        match self.function_control().clone() {
             Some(e) => (e.clone())(pipe_u),
             None => PipeState::NextState,
         }
     }
 
-    fn set_control<U: Fn(&T) -> PipeState + Send + Sync + 'static>(&mut self,func: U) -> &Self {
-        self.function_control = Some(Arc::new(func));
-        self
-    }
+    fn consume(&self,pipe_u: &T) -> PipeState {
 
-    fn set_consume<U: Fn(&T) -> PipeState + Send + Sync + 'static>(&mut self,func: U) -> &Self {
-        self.function_core = Some(Arc::new(func));
-        self
-    }
-
-    fn set_internal_state(&mut self, state: PipeState) {
-        self.internal_state = state;
+        match self.function_core().clone() {
+            Some(e) => (e.clone())(pipe_u),
+            None => panic!("Don't have function to consume"),
+        }
     }
 }
 
-struct Pipe<'r, T: PipeU> {
-    pipeArrayBox: Vec<PipeBox<T>>,
+struct Pipe<'r, T: PipeU, U: PipeB<T>> {
+    pipeArrayBox: Vec<U>,
     pipeArrayUsr: Vec<(&'r T,usize)>,
 }
 
-impl<'r,T> Pipe<'r,T> where T : PipeU
+impl<'r,T,U> Pipe<'r,T,U> where T : PipeU, U : PipeB<T>
 {
     fn new() -> Self {
         Pipe {
@@ -104,13 +79,13 @@ impl<'r,T> Pipe<'r,T> where T : PipeU
         }
     }
 
-    fn push_box(mut self,pipe_box: PipeBox<T>) -> Self {
+    fn push_box(mut self,pipe_box: U) -> Self {
         self.pipeArrayBox.push(pipe_box);
         self
     }
 
-    fn get_box(&self, pipe_u: &T) -> &PipeBox<T> {
-        let index_box: Option<&PipeBox<T>> = match self.pipeArrayUsr.iter().find(|x| x.0 == pipe_u) {
+    fn get_box(&self, pipe_u: &T) -> &U {
+        let index_box: Option<&U> = match self.pipeArrayUsr.iter().find(|x| x.0 == pipe_u) {
             Some(e) => Some(&self.pipeArrayBox[e.1]),
             None => panic!("Don't have this box for user"),
         };
@@ -169,67 +144,25 @@ impl<'r,T> Pipe<'r,T> where T : PipeU
 mod tests {
 
     use crate::Pipe;
-    use crate::PipeBox;
-    use crate::PipeU;
+    use crate::model::models::PipeBox;
+    use crate::model::models::PipeUser;
     use crate::PipeState;
 
     #[test]
     fn it_works() {
 
-        struct User {
-            say: String
-        }
+        let user_01 = PipeUser::new("Hello, World !");
+        let user_02 = PipeUser::new("Il fait chaud ici !");
 
-        impl PartialEq for User {
-            fn eq(&self, other: &Self) -> bool {
-                self.say == other.say
-            }
-        }
+        let box_1 = PipeBox::new();
+        let box_2 = PipeBox::new();
 
-        impl Eq for User {}
-
-        impl PipeU for User {
-            type Item = User;
-
-            fn get_item(&self) -> &Self::Item{
-                self
-            }
-        }
-
-        let mut box_1 = PipeBox::<User>::new();
-        box_1.set_control(|x| {println!("1 :{}",x.say);PipeState::ConsumeState});
-        box_1.set_consume(|x| {println!("1 :{}",x.say);PipeState::NextState});
-
-        let mut box_2 = PipeBox::<User>::new();
-        box_2.set_control(|x| {println!("2 :{}",x.say);PipeState::ConsumeState});
-        box_2.set_consume(|x| {println!("2 :{}",x.say);PipeState::NextState});
-
-        let mut box_3 = PipeBox::<User>::new();
-        box_3.set_control(|x| {
-            match x.say.as_str() {
-                "Hello mother fucker" => {println!("3 : Quelle grossier personnage");PipeState::ConsumeState},
-                "Fucking ass hole" => {println!("3: Trou du cul toi même");PipeState::ConsumeState},
-                _ => {println!("Mouais pas compris ?");PipeState::ConsumeState},
-            }
-        });
-
-        box_3.set_consume(|x| {
-            match x.say.as_str() {
-                "Hello mother fucker" => {println!("3 : Ton cul c'est du poulet");PipeState::ConsumeState},
-                "Fucking ass hole" => {println!("3: Ok gogole");PipeState::ConsumeState},
-                _ => {println!("Mouais pas compris ?");PipeState::ConsumeState},
-            }
-        });
         
         let mut pipe = Pipe::new()
                             .push_box(box_1)
-                            .push_box(box_2)
-                            .push_box(box_3);
+                            .push_box(box_2);
 
-        let u_1 = User{say:String::from("Hello mother fucker")};
-        let u_2 = User{say:String::from("Fucking ass hole")};
-
-        pipe.push_user(&u_1);
-        pipe.push_user(&u_2);
+        pipe.push_user(&user_01);
+        pipe.push_user(&user_02);
     }
 }
---
+++ src/model.rs
@@ -0,0 +1,85 @@
+// Models PipeBox and PipeUser
+pub mod models {
+
+    use crate::PipeB;
+    use crate::PipeU;
+    use crate::PipeState;
+    use std::sync::Arc;
+
+
+    pub struct PipeBox {
+        function_control: Arc<dyn Fn(&PipeUser) -> PipeState + Send + Sync>,
+        function_core: Arc<dyn Fn(&PipeUser) -> PipeState + Send + Sync>,
+        internal_state: PipeState,
+    }
+
+    impl PipeB<PipeUser> for PipeBox {
+        fn function_control(&self) -> Option<Arc<dyn Fn(&PipeUser) -> PipeState + Send + Sync>> {
+            let func = self.function_control.clone();
+            Some(func)
+        }
+        fn function_core(&self) -> Option<Arc<dyn Fn(&PipeUser) -> PipeState + Send + Sync>> {
+            let func = self.function_core.clone();
+            Some(func)
+        }
+        fn internal_state(&self) -> PipeState {
+            self.internal_state.clone()
+        }
+    }
+
+    impl PipeBox {
+        pub fn new() -> Self {
+            PipeBox {
+                function_control: Arc::new(|x: &PipeUser| {println!("PipeBox Controle :{}",x.get_item());PipeState::ConsumeState}),
+                function_core: Arc::new(|x: &PipeUser| {println!("PipeBox Core :{}",x.get_item());PipeState::NextState}),
+                internal_state: PipeState::ConsumeState,
+            }
+        }
+
+        fn internal_state(&self) -> &PipeState {
+            &self.internal_state
+        }
+
+        fn set_control<U: Fn(&PipeUser) -> PipeState + Send + Sync + 'static>(mut self,func: U) -> Self {
+            self.function_control = Arc::new(func);
+            self
+        }
+
+        fn set_consume<U: Fn(&PipeUser) -> PipeState + Send + Sync + 'static>(mut self,func: U) -> Self {
+            self.function_core = Arc::new(func);
+            self
+        }
+
+        fn set_internal_state(&mut self, state: PipeState) {
+            self.internal_state = state;
+        }
+    }
+
+    pub struct PipeUser {
+        say: String
+    }
+
+    impl PartialEq for PipeUser {
+        fn eq(&self, other: &Self) -> bool {
+            self.say == other.say
+        }
+    }
+
+    impl Eq for PipeUser {}
+
+    impl PipeU for PipeUser {
+        type Item = String;
+
+        fn get_item(&self) -> &Self::Item{
+            &self.say
+        }
+    }
+
+    impl PipeUser {
+        pub fn new(say: &str) -> Self {
+            PipeUser{
+                say: say.to_string() 
+            }
+        }
+    }
+}
